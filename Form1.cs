99ea7275baeed93943810bf1aaf6259151c2ca69
Wang Pincheng <wpc.369623245@live.cn>
2017-07-07 13:04:27 +0800

    Create the project
    A personal toy project

---
+++ Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToyProject
+{
+    public partial class Form1 : Form
+    {
+        uint i = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            i += 1;
+            label1.Text = Convert.ToString(i);
+        }
+    }
+}
